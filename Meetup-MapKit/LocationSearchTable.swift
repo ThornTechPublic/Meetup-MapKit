1f60c784c7e9da13e0e231b24e39e00b5f2e66af
Robert Chen <robert.chen@thorntechnologies.com>
2016-04-27 21:10:44 -0400

    Search Table

---
+++ Meetup-MapKit/LocationSearchTable.swift
@@ -0,0 +1,19 @@
+//
+//  LocationSearchTable.swift
+//  Meetup-MapKit
+//
+//  Created by Robert Chen on 4/27/16.
+//  Copyright © 2016 Robert Chen. All rights reserved.
+//
+
+import UIKit
+
+class LocationSearchTable: UITableViewController {
+    
+}
+
+extension LocationSearchTable: UISearchResultsUpdating {
+    func updateSearchResultsForSearchController(searchController: UISearchController) {
+        
+    }
+}
--- Meetup-MapKit/ViewController.swift
+++ Meetup-MapKit/ViewController.swift
@@ -6,12 +6,26 @@ class ViewController: UIViewController {
     @IBOutlet weak var mapView: MKMapView!
     
     let locationManager = CLLocationManager()
+
+    var resultSearchController: UISearchController? = nil
+    
+    func setupSearchTable(){
+        guard let locationSearchTable = storyboard?.instantiateViewControllerWithIdentifier(String(LocationSearchTable)) as? LocationSearchTable else { return }
+        resultSearchController = UISearchController(searchResultsController: locationSearchTable)
+        resultSearchController?.searchResultsUpdater = locationSearchTable
+        resultSearchController?.hidesNavigationBarDuringPresentation = false
+        let searchBar = resultSearchController?.searchBar
+        searchBar?.placeholder = "Search for places"
+        navigationItem.titleView = searchBar
+        definesPresentationContext = true
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         locationManager.delegate = self
         locationManager.requestWhenInUseAuthorization()
         locationManager.requestLocation()
+        setupSearchTable()
     }
 
 }
